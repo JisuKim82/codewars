3ee84c0d90a4a92c55687d62e8e253543a2eca09
Jisu Kim <jisu.kim@flatironschool.com>
2014-05-11 19:08:36 -0400

    Add test specs

--- spec/ipv4_to_int32_spec.rb
+++ spec/ipv4_to_int32_spec.rb
@@ -1,2 +1,10 @@
 require_relative 'spec_helper'
 require_relative '../ipv4_to_int32'
+
+describe '#ip_to_int32' do
+  context 'takes an IPv4 address and returns a 32 bit number' do
+    it 'should return 2149583361 when 128.32.10.1 is passed' do
+      expect(ip_to_int32('128.32.10.1')).to eq 2149583361   
+    end
+  end
+end
