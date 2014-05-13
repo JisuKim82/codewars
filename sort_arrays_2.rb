16e6d8626b5fdb7c18cb6350b476066ecc089272
Jisu Kim <jisu.kim@flatironschool.com>
2014-05-12 22:06:47 -0400

    Initial commit

---
+++ sort_arrays_2.rb
@@ -0,0 +1,8 @@
+# Simple sort, but this time sort regardless of upper / lower case. 
+# So the input of
+
+# [ "Hello", "there", "I'm", "fine"]
+# is translated to
+
+# ["fine", "Hello", "I'm", "there" ]
+
