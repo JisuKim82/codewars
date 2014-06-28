36f146351d6ae7a2e5017e4d60456e4787b4015e
Jisu Kim <jisu.kim@flatironschool.com>
2014-06-27 23:05:04 -0400

    Refactor solution

--- number_of_trailing_zeros_of_n.rb
+++ number_of_trailing_zeros_of_n.rb
@@ -6,21 +6,34 @@
 # that has 2 trailing zeros 4790016(00)
 # Be careful 1000! has length of 2568 digital numbers.
 
+#1st Solution
+# def zeros(n)
+#   digits = factorial(n).to_s.split('')
+#   zeros=0
+#   while digits[-1] == '0'
+#     zeros += 1
+#     digits.pop
+#   end
+#   zeros
+# end
+
+
+# def factorial(n)
+#   result = 1
+#   (1..n).each do |x|
+#     result *= x
+#   end
+#   result
+# end
+
+#2nd Solution to speed up the code
+
 def zeros(n)
-  digits = factorial(n).to_s.split('')
-  zeros=0
-  while digits[-1] == '0'
-    zeros += 1
-    digits.pop
-  end
-  zeros
-end
-
-
-def factorial(n)
-  result = 1
-  (1..n).each do |x|
-    result *= x
-  end
-  result
+   x = 5
+   counter = 0
+   while n/x >= 1
+     p counter += n/x
+     x*=5 
+   end 
+  counter
 end
